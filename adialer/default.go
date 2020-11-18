f4d82b1c699e00a8262a0bc80843e2c571394d85
rikaaa0928 <w1zirn1996@gmail.com>
2020-11-18 11:38:39 +0800

    awsl client

--- adialer/default.go
+++ adialer/default.go
@@ -4,7 +4,7 @@ func DefaultDialMids(d ADialer, ty string) ADialer {
 	switch ty {
 	case "awsl", "tcp":
 		d = NewAddrDataMid(d)
-		d = NewAuthDataMid(d)
+		//d = NewAuthDataMid(d)
 	default:
 	}
 	d = NewSendDataMid(d)
