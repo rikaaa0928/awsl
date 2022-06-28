0dd131793c35cf9daf9867246d5c6453a72af474
rikaaa0928 <w1zirn1996@gmail.com>
2022-06-28 16:17:37 +0800

    update arm64 darwin

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 GOOS=windows GOARCH=amd64 go build -o awsl.exe
 GOOS=linux GOARCH=amd64 go build -o awsl
-GOOS=darwin GOARCH=amd64 go build -o awsl_osx
+GOOS=darwin GOARCH=amd64 go build -o awsl_darwin_amd64
+GOOS=darwin GOARCH=arm64 go build -o awsl_darwin_arm64
